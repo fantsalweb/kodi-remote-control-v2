66ecf3f7fd633613d2e167920419df0c7a0ef3b2
Francisco Javier Antolinez Salazar <narf.27@gmail.com>
2025-10-06 19:37:07 +0200

    Add kodi-aside component

--- js/components/kodi-aside.js
+++ js/components/kodi-aside.js
@@ -1,11 +1,11 @@
 import { LitElement, html, css } from '../lib/lit.min.js';
 
 class KodiAside extends LitElement {
-  static styles = css
+  static styles = css`
     aside { width: 200px; background: #f0f0f0; }
-  ;
+  `;
   render() {
-    return html<aside>Sidebar content</aside>;
+    return html`<aside>Sidebar content</aside>`;
   }
 }
 customElements.define('kodi-aside', KodiAside);
